cc08a38c71fca094ef37838a4aa680e2f113b7fe
Blue_water_okie <54322474+okie-creator@users.noreply.github.com>
2024-09-08 21:49:40 -0500

    added some test cases

--- tests/test_core.py
+++ tests/test_core.py
@@ -23,13 +23,122 @@ class TestGraphOperations(unittest.TestCase):
         file_path = os.path.join(current_dir, '..', 'data', 'sample_graph.txt')
         
 
-        graph = read_graph_from_file(file_path)
+        edges = [
+        ("A", "B"), ("A", "C"), ("B", "D"), ("B", "E"), ("C", "F"), ("C", "G"),
+        ("D", "H"), ("E", "I"), ("F", "J"), ("G", "K"), ("H", "L"), ("I", "M"),
+        ("J", "N"), ("K", "O"), ("L", "P"), ("M", "Q"), ("N", "R"), ("O", "S"),
+        ("P", "T"), ("A", "D"), ("B", "F"), ("C", "H"), ("D", "J"), ("E", "K"),
+        ("F", "L"), ("G", "M"), ("H", "N"), ("I", "O"), ("J", "P"), ("K", "Q")
+        ]
+
+
+        graph = Graph()
+
+        vertices = {v for edge in edges for v in edge}
+        for v in vertices:
+            graph.add_vertex(v)
+
+        for v1, v2 in edges:
+            graph.add_edge(v1, v2)
         
 
         outputGraph = read_graph_from_file(file_path)
 
         self.assertEqual(graph, outputGraph)
+    def test_remove_edge(self):
 
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        
+
+        file_path = os.path.join(current_dir, '..', 'data', 'sample_graph.txt')
+        
+
+        edges = [
+        ("A", "B"), ("A", "C"), ("B", "D"), ("B", "E"), ("C", "F"), ("C", "G"),
+        ("D", "H"), ("E", "I"), ("F", "J"), ("G", "K"), ("H", "L"), ("I", "M"),
+        ("J", "N"), ("K", "O"), ("L", "P"), ("M", "Q"), ("N", "R"), ("O", "S"),
+        ("P", "T"), ("A", "D"), ("B", "F"), ("C", "H"), ("D", "J"), ("E", "K"),
+        ("F", "L"), ("G", "M"), ("H", "N"), ("I", "O"), ("J", "P"), ("K", "Q")
+        ]
+
+
+        graph = Graph()
+
+        vertices = {v for edge in edges for v in edge}
+        for v in vertices:
+            graph.add_vertex(v)
+
+        for v1, v2 in edges:
+            graph.add_edge(v1, v2)
+        
+        graph.remove_edge('A', 'B')
+
+        edges = [
+            ("A", "C"), ("B", "D"), ("B", "E"), ("C", "F"), ("C", "G"),
+            ("D", "H"), ("E", "I"), ("F", "J"), ("G", "K"), ("H", "L"), ("I", "M"),
+            ("J", "N"), ("K", "O"), ("L", "P"), ("M", "Q"), ("N", "R"), ("O", "S"),
+            ("P", "T"), ("A", "D"), ("B", "F"), ("C", "H"), ("D", "J"), ("E", "K"),
+            ("F", "L"), ("G", "M"), ("H", "N"), ("I", "O"), ("J", "P"), ("K", "Q")
+        ]
+
+
+        outGraph = Graph()
+
+        vertices = {v for edge in edges for v in edge}
+        for v in vertices:
+            outGraph.add_vertex(v)
+
+        for v1, v2 in edges:
+            outGraph.add_edge(v1, v2)
+
+        self.assertEqual(graph, outGraph)
+    def test_remove_vertice(self):
+
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        
+
+        file_path = os.path.join(current_dir, '..', 'data', 'sample_graph.txt')
+        
+
+        edges = [
+        ("A", "B"), ("A", "C"), ("B", "D"), ("B", "E"), ("C", "F"), ("C", "G"),
+        ("D", "H"), ("E", "I"), ("F", "J"), ("G", "K"), ("H", "L"), ("I", "M"),
+        ("J", "N"), ("K", "O"), ("L", "P"), ("M", "Q"), ("N", "R"), ("O", "S"),
+        ("P", "T"), ("A", "D"), ("B", "F"), ("C", "H"), ("D", "J"), ("E", "K"),
+        ("F", "L"), ("G", "M"), ("H", "N"), ("I", "O"), ("J", "P"), ("K", "Q")
+        ]
+
+
+        graph = Graph()
+
+        vertices = {v for edge in edges for v in edge}
+        for v in vertices:
+            graph.add_vertex(v)
+
+        for v1, v2 in edges:
+            graph.add_edge(v1, v2)
+        
+        graph.remove_vertex('A')
+
+        edges = [
+            ("B", "D"), ("B", "E"), ("C", "F"), ("C", "G"),
+            ("D", "H"), ("E", "I"), ("F", "J"), ("G", "K"), ("H", "L"), ("I", "M"),
+            ("J", "N"), ("K", "O"), ("L", "P"), ("M", "Q"), ("N", "R"), ("O", "S"),
+            ("P", "T"), ("B", "F"), ("C", "H"), ("D", "J"), ("E", "K"),
+            ("F", "L"), ("G", "M"), ("H", "N"), ("I", "O"), ("J", "P"), ("K", "Q")
+        ]
+
+
+        outGraph = Graph()
+
+        vertices = {v for edge in edges for v in edge}
+        for v in vertices:
+            outGraph.add_vertex(v)
+
+        for v1, v2 in edges:
+            outGraph.add_edge(v1, v2)
+
+        self.assertEqual(graph, outGraph)
 if __name__ == '__main__':
     unittest.main()
 
