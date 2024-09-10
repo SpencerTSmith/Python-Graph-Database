cc1acff38f509592ecd5aaae2d2dd5354c6a5552
Blue_water_okie <54322474+okie-creator@users.noreply.github.com>
2024-09-10 13:17:08 -0500

    added test cases

--- tests/test_core.py
+++ tests/test_core.py
@@ -43,8 +43,14 @@ class TestGraphOperations(unittest.TestCase):
         
 
         outputGraph = read_graph_from_file(file_path)
-
+        graphO = GraphOperations(graph)
+        graphI = GraphOperations(outputGraph)
         self.assertEqual(graph, outputGraph)
+        self.assertEqual(graph.get_all_vertices(), outputGraph.get_all_vertices())
+        self.assertEqual(graph.has_edge("A", "B"), outputGraph.has_edge("A", "B"))
+        self.assertEqual(graphO, graphI)
+        self.assertEqual(graphO.get_neighbors("A"), graphI.get_neighbors("A"))
+        self.assertEqual(graphO.get_shortest_path("A", "G"), graphI.get_shortest_path("A", "G"))
     def test_remove_edge(self):
 
         current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -139,6 +145,33 @@ class TestGraphOperations(unittest.TestCase):
             outGraph.add_edge(v1, v2)
 
         self.assertEqual(graph, outGraph)
+
+    def test_has_neighbors(self):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        
+
+        file_path = os.path.join(current_dir, '..', 'data', 'sample_graph.txt')
+        
+
+        edges = [
+        ("A", "B"), ("A", "C"), ("B", "D"), ("B", "E"), ("C", "F"), ("C", "G"),
+        ("D", "H"), ("E", "I"), ("F", "J"), ("G", "K"), ("H", "L"), ("I", "M"),
+        ("J", "N"), ("K", "O"), ("L", "P"), ("M", "Q"), ("N", "R"), ("O", "S"),
+        ("P", "T"), ("A", "D"), ("B", "F"), ("C", "H"), ("D", "J"), ("E", "K"),
+        ("F", "L"), ("G", "M"), ("H", "N"), ("I", "O"), ("J", "P"), ("K", "Q")
+        ]
+
+
+        graph = Graph()
+
+        vertices = {v for edge in edges for v in edge}
+        for v in vertices:
+            graph.add_vertex(v)
+
+        for v1, v2 in edges:
+            graph.add_edge(v1, v2)
+
 if __name__ == '__main__':
     unittest.main()
+
 
